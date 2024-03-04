bb9da9a604d17e0db9b227c3d38bad5f3a49f9dd
Joris Bertier <joris.bertier@gmail.com>
2024-03-04 09:30:50 -0500

    mise en place environnement de travail

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import Wrapper from './Wrapper';
+
+function App() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/components/Wrapper.js
@@ -0,0 +1,9 @@
+import '../styles/wrapper.css'
+
+function Wrapper() {
+    return <div className='wrapper'>
+
+    </div>
+}
+
+export default Wrapper
